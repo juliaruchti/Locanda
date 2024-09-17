ba8df10c85a650edd2bb56407c94d5ae0262f9a0
julia ruchti <127301814+juliaruchti@users.noreply.github.com>
2024-09-17 09:53:08 +0000

    feed, blog, footer & impressum

--- index.js
+++ index.js
@@ -13,8 +13,34 @@ app.get("/", async function (req, res) {
   res.render("start", {});
 });
 
+/* Feed */
+app.get("/feed", async function (req, res) {
+  res.render("feed", {});
+});
+
+/* Blog */
+app.get("/blog", async function (req, res) {
+  res.render("blog", {});
+});
+
+/* Favourites */
+app.get("/favourites", async function (req, res) {
+  res.render("favourites", {});
+});
+
+/* Account */
+app.get("/account", async function (req, res) {
+  res.render("account", {});
+});
+
+/* Impressum */
 app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get("*", (req, res) => {
+  res.render("404", {});
 });
 
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
