709e08afe0bc2040241ceb87458af606e15af6de
julia ruchti <127301814+juliaruchti@users.noreply.github.com>
2024-11-19 10:58:28 +0000

    Dateiupload

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ const app = createApp({
 
 /* New Post */
 app.get("/new_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   res.render("new_post", {});
 });
 
 /* Start */
-app.get("/start", async function (req, res) {
+app.get("/", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   const posts = await app.locals.pool.query(
     "SELECT posts.*, users.username FROM posts INNER JOIN users ON posts.user_id = users.id;"
   );
@@ -39,19 +47,31 @@ app.get("/login", async function (req, res) {
 
 /* new_post */
 app.get("/new_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   res.render("new_post", {});
 });
 
 app.post("/create_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   await app.locals.pool.query(
     "INSERT INTO posts (title, contents, country, created_at) VALUES ($1, $2, $3, current_timestamp)",
     [req.body.title, req.body.contents, req.body.country]
   );
-  res.redirect("/start");
+  res.redirect("/");
 });
 
 /* Blog */
 app.get("/blog/:id", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   const posts = await app.locals.pool.query(
     `SELECT posts.*, users.username FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.id = ${req.params.id};`
   );
@@ -64,6 +84,10 @@ app.get("/blog/:id", async function (req, res) {
 
 /* Favourites */
 app.get("/favourites", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   const favourites = await app.locals.pool.query("SELECT * FROM favourites");
   const images = await app.locals.pool.query("SELECT * FROM images");
   res.render("favourites", {
@@ -74,6 +98,10 @@ app.get("/favourites", async function (req, res) {
 
 /* Account */
 app.get("/account", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   const users = await app.locals.pool.query("SELECT * FROM account");
   res.render("account", { users: users.rows });
 });
